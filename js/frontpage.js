8c3a6c9b3ecf74e065c48d3b2d86da5d29f134d7
Joonas <>
2021-11-07 19:14:50 +0200

    Add js

---
+++ js/frontpage.js
@@ -0,0 +1,13 @@
+function firstFunction(){
+    var foo = document.getElementById("stockValue").value
+    var fullstring = `fetch?stockToFetch=${foo}`
+    
+    window.location.replace(fullstring)
+}
+
+function searchFunction(){
+    var foo = document.getElementById("stockSymbol").value
+    var fullstring = `by-stocksymbol?stockSymbol=${foo}`
+    
+    window.location.replace(fullstring)
+}
